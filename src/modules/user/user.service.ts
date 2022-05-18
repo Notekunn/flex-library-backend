41c148c3ca7bb6d1d34735eaca8cb08850ac5ae6
DC <cuong.nl5.kma@gmail.com>
2022-05-18 21:33:25 +0700

    feat: draft: crud user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,23 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entities/user.entity';
+import { UserRepository } from './repositories/user.repository';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: UserRepository,
+  ) {}
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user' + createUserDto;
+    return this.userRepository.save(this.userRepository.create(createUserDto));
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.userRepository.findOne({
+      where: {
+        id,
+      },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user` + updateUserDto;
+    return {
+      id,
+      updateUserDto,
+    };
   }
 
   remove(id: number) {
