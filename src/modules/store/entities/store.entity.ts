bf20feece0e826eda8cfdce82cf408e4a4c6d93c
DC <cuong.nl5.kma@gmail.com>
2022-05-30 16:33:33 +0700

    feat: add store lat lng columns

--- src/modules/store/entities/store.entity.ts
+++ src/modules/store/entities/store.entity.ts
@@ -1,6 +1,6 @@
 import { AbstractEntity } from '@common/abstract.entity';
 import { UserEntity } from '@modules/user/entities/user.entity';
-import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+import { Column, Entity, Index, ManyToOne, OneToMany } from 'typeorm';
 import { BookshelfEntity } from './bookshelf.entity';
 
 @Entity({ name: 'store' })
@@ -8,11 +8,23 @@ export class StoreEntity extends AbstractEntity {
   @ManyToOne(() => UserEntity, (user) => user.stores)
   owner: UserEntity;
 
+  @Index()
   @Column({ type: 'varchar' })
   name: string;
 
   @Column({ type: 'varchar' })
   address: string;
+
+  @Index()
+  @Column({ type: 'double precision', nullable: true })
+  latitude?: number;
+
+  @Index()
+  @Column({ type: 'double precision', nullable: true })
+  longitude?: number;
+
+  @Column({ nullable: true })
+  provinceId?: number;
 
   @OneToMany(() => BookshelfEntity, (storeBook) => storeBook.book)
   bookshelfs: BookshelfEntity[];
