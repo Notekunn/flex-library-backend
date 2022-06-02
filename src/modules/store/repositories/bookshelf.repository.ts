e8685dbb03edb4930f6f5772e2d4d318e7c1a597
DC <cuong.nl5.kma@gmail.com>
2022-06-02 14:17:09 +0700

    fix: rename bookshelf repository

--- src/modules/store/repositories/bookshelf.repository.ts
+++ src/modules/store/repositories/bookshelf.repository.ts
@@ -1,4 +1,4 @@
 import { Repository } from 'typeorm';
 import { BookshelfEntity } from '../entities/bookshelf.entity';
 
-export class StoreBookRepository extends Repository<BookshelfEntity> {}
+export class BookshelfRepository extends Repository<BookshelfEntity> {}
