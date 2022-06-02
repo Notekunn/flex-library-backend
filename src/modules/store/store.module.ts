aba56a7abf943d1031c055596554f5536d7d741d
DC <cuong.nl5.kma@gmail.com>
2022-06-02 14:11:32 +0700

    fix: split bookshelf command query

--- src/modules/store/store.module.ts
+++ src/modules/store/store.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { StoreController } from './controllers/store.controller';
-import { StoreCommandHandlers } from './commands';
-import { StoreQueryHandlers } from './queries';
+import { BookshelfCommandHandlers, StoreCommandHandlers } from './commands';
+import { BookshelfQueryHandlers, StoreQueryHandlers } from './queries';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CqrsModule } from '@nestjs/cqrs';
 import { StoreEntity } from './entities/store.entity';
 import { BookshelfEntity } from './entities/bookshelf.entity';
-import { StoreBookController } from './controllers/bookshelf.controller';
+import { BookshelfController } from './controllers/bookshelf.controller';
 
 @Module({
-  controllers: [StoreController, StoreBookController],
-  providers: [...StoreCommandHandlers, ...StoreQueryHandlers],
+  controllers: [StoreController, BookshelfController],
+  providers: [...StoreCommandHandlers, ...BookshelfCommandHandlers, ...StoreQueryHandlers, ...BookshelfQueryHandlers],
   imports: [TypeOrmModule.forFeature([StoreEntity, BookshelfEntity]), CqrsModule],
 })
 export class StoreModule {}
