d69480fb86f77451133b1b02afc398f4cdb8aff4
DC <cuong.nl5.kma@gmail.com>
2022-06-06 06:46:46 +0700

    fix: one user is only register one store

--- src/modules/store/commands/create-store.command.ts
+++ src/modules/store/commands/create-store.command.ts
@@ -1,10 +1,12 @@
 import { GetOneUserQuery } from '@modules/user/queries/get-one-user.query';
 import { Command } from '@nestjs-architects/typed-cqrs';
-import { NotFoundException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { CommandHandler, ICommandHandler, QueryBus } from '@nestjs/cqrs';
 import { InjectRepository } from '@nestjs/typeorm';
+import { I18nService } from 'nestjs-i18n';
 import { CreateStoreDto } from '../dto/create-store.dto';
 import { StoreEntity } from '../entities/store.entity';
+import { GetStoreByOwnerQuery } from '../queries/get-store-by-owner';
 import { StoreRepository } from '../repositories/store.repository';
 
 export class CreateStoreCommand extends Command<CreateStoreDto> {
@@ -15,14 +17,20 @@ export class CreateStoreCommand extends Command<CreateStoreDto> {
 @CommandHandler(CreateStoreCommand)
 export class CreateStoreCommandHandler implements ICommandHandler<CreateStoreCommand> {
   constructor(
-    @InjectRepository(StoreEntity) private readonly storeRepository: StoreRepository,
+    @InjectRepository(StoreEntity)
+    private readonly storeRepository: StoreRepository,
     private readonly queryBus: QueryBus,
+    private readonly i18n: I18nService,
   ) {}
   async execute(command: CreateStoreCommand) {
     const { dto, ownerId } = command;
     const user = await this.queryBus.execute(new GetOneUserQuery(ownerId));
     if (!user) {
-      throw new NotFoundException('User not found');
+      throw new NotFoundException(this.i18n.t('exception.userNotFound'));
+    }
+    const existedStore = await this.queryBus.execute(new GetStoreByOwnerQuery(ownerId));
+    if (existedStore) {
+      throw new BadRequestException(this.i18n.t('message.duplicateStore'));
     }
     const store = this.storeRepository.create(dto);
     store.owner = user;
