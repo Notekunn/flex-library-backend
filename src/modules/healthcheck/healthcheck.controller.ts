a2b5a63ff7344631b9cfc3f5b8e435c29c1a6778
DC <cuong.nl5.kma@gmail.com>
2022-06-18 19:03:06 +0700

    fix: semantic release don't change version in package.json

--- src/modules/healthcheck/healthcheck.controller.ts
+++ src/modules/healthcheck/healthcheck.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Logger } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { HealthcheckResponse } from './dto/healthcheck-response.dto';
+import { version } from '../../../package.json';
 
 @Controller('healthcheck')
 @ApiTags('system')
@@ -13,7 +14,7 @@ export class HealthcheckController {
     this.logger.log('heathcheck');
 
     return {
-      version: 'v1.0.0',
+      version,
     };
   }
 }
