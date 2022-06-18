adb6241505bb8d8e205568ae7033f3c74b4796a7
DC <cuong.nl5.kma@gmail.com>
2022-06-18 18:54:15 +0700

    fix: presistAuthorization fastify swagger

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,5 +1,5 @@
 import { INestApplication } from '@nestjs/common';
-import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, FastifySwaggerCustomOptions, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
 
 export function setupSwagger(app: INestApplication): void {
   const options = new DocumentBuilder()
@@ -9,15 +9,10 @@ export function setupSwagger(app: INestApplication): void {
     .addTag('auth', 'Authenticate user')
     .addTag('user', 'Manage user account')
     .build();
-  const customOptions: SwaggerCustomOptions = {
-    swaggerOptions: {
+  const customOptions: FastifySwaggerCustomOptions = {
+    uiConfig: {
       persistAuthorization: true,
-      // requestInterceptor: (req) => {
-      //   req.credentials = 'include';
-      //   return req;
-      // },
     },
-    customSiteTitle: 'Flex Library API Docs',
   };
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document, customOptions);
