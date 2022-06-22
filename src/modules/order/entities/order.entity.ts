aa5b5cf6b24d96fd241f5f07646d5847f6be7c83
DC <cuong.nl5.kma@gmail.com>
2022-06-22 23:05:19 +0700

    fix: order entity

--- src/modules/order/entities/order.entity.ts
+++ src/modules/order/entities/order.entity.ts
@@ -2,15 +2,17 @@ import { AbstractEntity } from '@common/abstract.entity';
 import { OrderStatus } from '@constants/order-status.enum';
 import { StoreEntity } from '@modules/store/entities/store.entity';
 import { UserEntity } from '@modules/user/entities/user.entity';
-import { Column, Entity, ManyToOne, OneToMany, OneToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne } from 'typeorm';
 import { OrderDetailEntity } from './order-detail.entity';
 
 @Entity('order')
 export class OrderEntity extends AbstractEntity {
   @OneToOne(() => UserEntity, (user) => user.id)
+  @JoinColumn()
   user: UserEntity;
 
   @ManyToOne(() => StoreEntity, (store) => store.id)
+  @JoinColumn()
   store: StoreEntity;
 
   @Column({ enum: OrderStatus, default: OrderStatus.CREATED })
