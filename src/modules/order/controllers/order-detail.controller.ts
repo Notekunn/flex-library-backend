9f41185116afae45b8933f7f015a93ef1b29bceb
DC <cuong.nl5.kma@gmail.com>
2022-06-27 00:50:06 +0700

    fix: order detail controller

--- src/modules/order/controllers/order-detail.controller.ts
+++ src/modules/order/controllers/order-detail.controller.ts
@@ -1,27 +1,12 @@
-import { PaginationDto } from '@common/dto/pagination.dto';
 import { AuthUser } from '@decorators/auth-user.decorator';
 import { JwtAuthGuard } from '@guards/jwt-auth.guard';
 import { JwtClaimsDto } from '@modules/auth/dto/jwt-claims.dto';
-import {
-  Body,
-  ClassSerializerInterceptor,
-  Controller,
-  Get,
-  Param,
-  ParseIntPipe,
-  Post,
-  Query,
-  UseGuards,
-  UseInterceptors,
-  ValidationPipe,
-} from '@nestjs/common';
-import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { Body, ClassSerializerInterceptor, Controller, Post, UseGuards, UseInterceptors } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateOrderDetailCommand } from '../commands/create-order-detail.command';
 import { CreateOrderDetailDto } from '../dto/create-order-detail.dto';
 import { OrderEntity } from '../entities/order.entity';
-import { GetAllOrderDetailQuery } from '../queries/get-all-order-detail.query';
-import { GetOneOrderDetailQuery } from '../queries/get-one-order-detail.query';
 
 @ApiTags('order-detail')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -29,7 +14,7 @@ import { GetOneOrderDetailQuery } from '../queries/get-one-order-detail.query';
 @ApiBearerAuth()
 @Controller('order-detail')
 export class OrderDetailController {
-  constructor(private readonly queryBus: QueryBus, private readonly commandBus: CommandBus) {}
+  constructor(private readonly commandBus: CommandBus) {}
   @Post()
   @ApiResponse({
     type: OrderEntity,
@@ -37,9 +22,5 @@ export class OrderDetailController {
   })
   async createOrderDetail(@AuthUser() user: JwtClaimsDto, @Body() createOrderDetailDto: CreateOrderDetailDto) {
     return this.commandBus.execute(new CreateOrderDetailCommand(user.id, createOrderDetailDto));
-  }
-  @Get(':id')
-  async getOneOrderDetail(@Param('id', ParseIntPipe) id: number) {
-    return this.queryBus.execute(new GetOneOrderDetailQuery(id));
   }
 }
