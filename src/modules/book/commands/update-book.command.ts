6f67660b4fc91d3b16139723d2cd72c5e686ccaf
DC <cuong.nl5.kma@gmail.com>
2022-07-13 22:13:02 +0700

    feat: update category of book

--- src/modules/book/commands/update-book.command.ts
+++ src/modules/book/commands/update-book.command.ts
@@ -1,7 +1,10 @@
+import { CategoryEntity } from '@modules/category/entities/category.entity';
+import { GetOneCategoryQuery } from '@modules/category/queries/get-one-category.query';
 import { Command } from '@nestjs-architects/typed-cqrs';
 import { NotFoundException } from '@nestjs/common';
-import { CommandBus, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandBus, CommandHandler, ICommandHandler, QueryBus } from '@nestjs/cqrs';
 import { InjectRepository } from '@nestjs/typeorm';
+import { I18nService } from 'nestjs-i18n';
 import { UpdateBookDto } from '../dto/update-book.dto';
 import { BookEntity } from '../entities/book.entity';
 import { BookRepository } from '../repositories/book.repository';
@@ -16,20 +19,35 @@ export class UpdateBookCommand extends Command<UpdateBookDto> {
 @CommandHandler(UpdateBookCommand)
 export class UpdateBookCommandHandler implements ICommandHandler<UpdateBookCommand> {
   constructor(
-    @InjectRepository(BookEntity) private readonly bookRepository: BookRepository,
+    @InjectRepository(BookEntity)
+    private readonly bookRepository: BookRepository,
     private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+    private readonly i18n: I18nService,
   ) {}
   async execute(command: UpdateBookCommand) {
     const { id, dto } = command;
+    console.log(dto);
+
+    const { categoryIds, ...dataToUpdate } = dto;
     const book = await this.bookRepository.findOne({ where: { id } });
     if (!book) {
       throw new NotFoundException('Book not found');
     }
-    const updatedBook = await this.bookRepository.save({
+    const updatedBook = await this.bookRepository.create({
       ...book,
-      ...dto,
+      ...dataToUpdate,
     });
-    await this.commandBus.execute(new UpdateNumberOfCopiesCommand(id));
+
+    // TODO: Implement p-limit
+    if (categoryIds) {
+      const categories = await Promise.all(categoryIds.map((id) => this.queryBus.execute(new GetOneCategoryQuery(id))));
+      updatedBook.categories = categories.filter((category) => category) as CategoryEntity[];
+    }
+    await Promise.all([
+      this.bookRepository.save(updatedBook),
+      this.commandBus.execute(new UpdateNumberOfCopiesCommand(id)),
+    ]);
 
     return updatedBook;
   }
